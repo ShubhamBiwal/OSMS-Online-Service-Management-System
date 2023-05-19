3707856498d00874c98dde461dea9cd80880ffde
Shubham Kumar <100457482+ShubhamBiwal@users.noreply.github.com>
2023-05-19 20:23:08 +0530

    Update password-reset.php

--- technician/password-reset.php
+++ technician/password-reset.php
@@ -20,13 +20,13 @@ function sendMail($email, $code)
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'shubhambiwa@gmail.com';                     //SMTP username
-        $mail->Password   = 'layllemfzdliahlg';                               //SMTP password
+        $mail->Username   = '';                     //SMTP username
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('shubhambiwa@gmail.com', 'OSMS');
+        $mail->setFrom('', 'OSMS');
         $mail->addAddress($email);     //Add a recipient
 
         //Content
