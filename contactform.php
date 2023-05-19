9cb5755795495e958cc98cab1f390ce83dcb42ed
Shubham Kumar <100457482+ShubhamBiwal@users.noreply.github.com>
2023-05-19 20:33:31 +0530

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -19,14 +19,14 @@
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'shubhambiwal21042003@gmail.com';                     //SMTP username
-            $mail->Password   = 'layllemfzdliahlg';                               //SMTP password
+            $mail->Username   = '';                     //SMTP username
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('shubhambiwal21042003@gmail.com', 'OSMS');
-            $mail->addAddress('shubhambiwal21042003@gmail.com');     //Add a recipient
+            $mail->setFrom('', 'OSMS');
+            $mail->addAddress('');     //Add a recipient
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
